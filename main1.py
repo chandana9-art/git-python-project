cf8d4e151a9ee2c0c0a32c76f958fed7c000bb1e
chandana9-art <chandanamandugula@gmail.com>
2021-11-08 20:41:00 +0530

    Bootcamp

---
+++ main1.py
@@ -0,0 +1,11 @@
+import math
+ def isPerfectSquare(x):
+     if(x>=0):
+          s=math.sqrt(X)
+          return ((s*s)==float(x))
+      return false
+x=int(input("enter no:"))
+if (isPerfectSquare(x)):
+      print("yes")
+else:
+      print("no")
